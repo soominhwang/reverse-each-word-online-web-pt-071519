96ffb2e3ce8c9a756b9e36766cb10d9fa6db8951
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-29 05:23:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   array = sentence.split(" ")
   new_array = []
-  array.each do |word|
+  array.collect do |word|
     new_array << word.reverse
   end
   new_array.join(" ")
